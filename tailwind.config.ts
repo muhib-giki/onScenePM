e0667fc81536ae2ae6fe70daaa57242cd55b312b
muhib-giki <muhibmhanif@gmail.com>
2025-11-15 02:32:24 +0500

    sidebar, navbar, darkmode, sidebar collapsing done

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,7 +12,7 @@ const config: Config = {
       colors: {
         white: "#ffffff",
         gray: {
-          100: "f3f4f6",
+          100: "#f3f4f6",
           200: "#efe7eb",
           300: "#d1d5db",
           500: "#6b7280",
